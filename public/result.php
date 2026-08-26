a398c719f260e3788ae26fb94de761baa906254c
Leonardo <leonardosch44@gmail.com>
2026-08-26 17:46:01 -0300

    changes in the folder organization

--- public/result.php
+++ public/result.php
@@ -21,7 +21,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultados</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header>
